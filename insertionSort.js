a3130fde25af979f08dff2e78319c87ab2d933f0
Andre Wilkinson <andre_w3@yahoo.com>
2024-06-19 10:28:02 -0400

    insert sort file created

--- insertionSort.js
+++ insertionSort.js
@@ -1,6 +1,11 @@
 function insertionSort(arr) {
-    for (let x = 0; x < arr.length; x++){
-        
+    // let newArr = []
+    // let secondEl = arr[1];
+    for (let i = 1; i < arr.length; i++){
+        let currentVal = arr[i];
+       
 
     }
+    return arr;
 }
+insertionSort([2, 1, 9, 76, 4])
