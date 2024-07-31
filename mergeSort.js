9408851e9b8abbdfd298cf5a0a99bbfb4dd85afa
Andre Wilkinson <andre_w3@yahoo.com>
2024-07-31 16:14:10 -0400

    merge

--- mergeSort.js
+++ mergeSort.js
@@ -1,4 +1,4 @@
-function mergeSort(arr1, arr2) {
+function merge(arr1, arr2) {
     let results = [];
     let i = 0;
     let j = 0;
@@ -9,8 +9,7 @@ function mergeSort(arr1, arr2) {
         } else {
             results.push(arr2[j])
             j++;
-        }
-        
+        }        
     }
     while (i < arr1.length) {
         results.push(arr1[i])
@@ -20,12 +19,15 @@ function mergeSort(arr1, arr2) {
         results.push(arr1[j])
         j++;
     }
-
     return results;
-
-
 }
 
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+}
 mergeSort([1, 10, 50], [2, 14, 99, 100])
 
 
