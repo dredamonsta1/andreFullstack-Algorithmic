58d18ef7c8c4f3c059bd5393a6363baaded2f476
Andre Wilkinson <andre_w3@yahoo.com>
2024-09-05 01:18:56 -0400

    linked list

--- linkedList.js
+++ linkedList.js
@@ -8,8 +8,24 @@ class Node{
     }
 }
 
-let first = new Node("Hi")
-first.next = new Node("there")
-first.next.next = new Node("how")
-first.next.next.next = new Node("are")
-first.next.next.next.next = new Node("you")
+class SinglyLinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        
+    }
+}
+
+// let first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+
+let list = new SinglyLinkedList()
+list.push("HElLO")
+list.push("GOODBYE")
+list.push("yooooooooooooo")
