7e95d30315b17273b67d8bf9e71a4451562305be
Andre Wilkinson <andre_w3@yahoo.com>
2024-09-05 02:05:23 -0400

    linked list

--- linkedList.js
+++ linkedList.js
@@ -15,7 +15,16 @@ class SinglyLinkedList{
         this.length = 0;
     }
     push(val) {
-        
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 }
 
