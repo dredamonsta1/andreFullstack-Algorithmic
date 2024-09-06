1ddef9b1358b244f9b35e6a8b60fcc4d0d198fbe
Andre Wilkinson <andre_w3@yahoo.com>
2024-09-06 12:09:32 -0400

    linked list pop

--- linkedList.js
+++ linkedList.js
@@ -29,6 +29,20 @@ class SinglyLinkedList{
     pop(val) {
         // for(Node)
         if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length == 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
     }
 }
 
