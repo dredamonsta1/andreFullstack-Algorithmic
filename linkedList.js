bc78c5f0f7380d3041a33d4f9c3a511743a0bf9e
Andre Wilkinson <andre_w3@yahoo.com>
2024-09-17 14:48:58 -0400

    singly linked list insert

--- linkedList.js
+++ linkedList.js
@@ -84,6 +84,13 @@ class SinglyLinkedList{
     insert(index, val) {
         if (index < 0 || index > this.length) return false;
         if (index === this.length) return this.push(val);
+        if (index === 0) return this.unshift(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
 
         }
 }
