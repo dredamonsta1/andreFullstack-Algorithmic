0eea427e50cf2d117e0297d86ddb7d62481b1316
Andre Wilkinson <andre_w3@yahoo.com>
2024-10-16 15:48:40 -0400

    extra four lines of code that gets rid of runtime error

--- minMaxSum.js
+++ minMaxSum.js
@@ -6,11 +6,14 @@ function miniMaxSum(arr) {
     let findLarge = arr.filter((array) => array !== largeFound)
     let findSmall = arr.filter((array) => array !== smallFound)
     // then used .reduce method to sum the new array
-    let largeArr = findLarge.reduce(function (x, y){
-        return x + y;
-    })
-    let smallArr = findSmall.reduce(function(x, y){
-        return x + y;
-    })
+    // let largeArr = findLarge.reduce(function (x, y){
+    //     return x + y;
+    // })
+    // let smallArr = findSmall.reduce(function(x, y){
+    //     return x + y;
+    // })
+    let largeArr = findLarge.reduce((x, y) =>  x += y, 0);
+    let smallArr = findSmall.reduce((x, y) => x += y, 0)
     console.log( largeArr, smallArr)
 }
+// runtime error on 55555 test case due to getting the same number for both
